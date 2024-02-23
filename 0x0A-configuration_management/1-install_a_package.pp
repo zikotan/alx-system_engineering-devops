45ab441056929420bfda34255624e48c96409b32
zikotan <tananzakaria630@gmail.com>
2024-02-24 00:54:57 +0100

    response

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# install flask -v 2.5.0
+# Install Flask version 2.1.0 using pip3
 
-exec { 'flask':
-  command => '/usr/bin/apt-get -y install flask -v 2.1.0',
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
