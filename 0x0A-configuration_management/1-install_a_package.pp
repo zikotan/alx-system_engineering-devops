3975811fe195c5ffdc88b8785bdb6c10ac9427af
zikotan <tananzakaria630@gmail.com>
2024-02-24 00:16:23 +0100

    response

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# install puppet-lint -v 2.5.0
+# Install puppet-lint version 2.5.0
+package { 'puppet-lint':
+    ensure => '2.5.0',
+    provider => 'apt',
+    require => Exec['update apt'],
+}
 
-exec { 'install puppet-lint':
-    command => '/usr/bin/apt-get -y install puppet-lint=2.5.0',
+exec { 'update apt':
+    command => '/usr/bin/apt-get update',
+    refreshonly => true,
 }
