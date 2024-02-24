326d3ebb10004087336833dd2b481e1456e88c21
zikotan <tananzakaria630@gmail.com>
2024-02-25 00:21:15 +0100

    response

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,5 @@
-# Ensure Flask version 2.1.0 is installed using pip3
+# Install Flask version 2.1.0 using pip3
 package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
-
-# Update the Werkzeug library if necessary
-package { 'werkzeug':
-  ensure   => 'latest',
-  provider => 'pip3',
-  require  => Package['Flask'],
+  ensure   =>  '2.1.0',
+  provider =>  'gem',
 }
