6490abcd4deb26b67f78ab495dab199d046dad5a
zikotan <tananzakaria630@gmail.com>
2024-04-09 22:00:11 +0000

    response

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """Function to query subscribers on a given Reddit subreddit."""
+
+
+
 import requests
 
 
@@ -9,6 +12,7 @@ def number_of_subscribers(subreddit):
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
+
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
