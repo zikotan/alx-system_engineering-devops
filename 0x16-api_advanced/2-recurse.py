16017e81c4dcc2fb0cf504855d4f5cc7defca57e
zikotan <tananzakaria630@gmail.com>
2024-04-09 22:09:24 +0000

    response

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,23 +1,26 @@
+#!/usr/bin/python3
+"""Contains recurse function"""
 import requests
 
+
 def recurse(subreddit, hot_list=[], after="", count=0):
+    """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)"
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
     }
     params = {
         "after": after,
         "count": count,
         "limit": 100
     }
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
     if response.status_code == 404:
         return None
 
     results = response.json().get("data")
-    if results is None:  # Check if results is None
-        return None
-
     after = results.get("after")
     count += results.get("dist")
     for c in results.get("children"):
