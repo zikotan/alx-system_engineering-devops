fce1d1657134bff60f72e53b3d2fb72d0e441dbe
zikotan <tananzakaria630@gmail.com>
2024-04-10 17:42:31 +0000

    response

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-'''
-    this module contains the function number_of_subscribers
-'''
-import requests
 from sys import argv
-
+import requests
 
 def number_of_subscribers(subreddit):
-    '''
-        returns the number of subscribers for a given subreddit
-    '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/about.json'
-                       .format(subreddit), headers=user).json()
+    """
+    Returns the number of subscribers for a given subreddit.
+    """
+    user_agent = {'User-Agent': 'Lizzie'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    
     try:
-        return url.get('data').get('subscribers')
+        response = requests.get(url, headers=user_agent, allow_redirects=False)
+        if response.status_code == 200:
+            return response.json().get('data', {}).get('subscribers', 0)
+        else:
+            return 0
     except Exception:
         return 0
 
-
 if __name__ == "__main__":
-    number_of_subscribers(argv[1])
+    if len(argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(argv[1])))
